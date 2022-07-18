a8919be8f780c46ab574a4dbf58e18cec79e0a66
정억화 <jeong-eoghwa@jeong-eoghwaui-MacBookPro.local>
2022-07-18 17:56:11 +0900

    fix: 초기셋팅 수정

--- src/Router.js
+++ src/Router.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
-import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
 
 function Router() {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Main />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/components/Sidemenu/Sidemenu.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Sidemenu = () => {
+  return <div />;
+};
+
+export default Sidemenu;
