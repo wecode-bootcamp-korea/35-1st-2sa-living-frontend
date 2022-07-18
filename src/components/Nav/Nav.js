93078afaf528a8218a0415dccf6b0be991a4c483
정억화 <jeong-eoghwa@jeong-eoghwaui-MacBookPro.local>
2022-07-18 22:11:36 +0900

    add: 메일페이지레이아웃

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,26 @@ import './Nav.scss';
 function Nav() {
   return (
     <nav>
-      <div className="inner">네비</div>
+      <div className="inner">
+        <div className="logo">
+          <img
+            src="https://www.casa.co.kr/images/common/logo_new.gif"
+            alt="logo"
+            width="99"
+          />
+        </div>
+        <ul className="menu">
+          <li>FURNITURE</li>
+          <li>HOMEWARE</li>
+          <li>LIGHTING</li>
+          <li>LIFE</li>
+        </ul>
+        <div className="icon-box">
+          <i class="fa-solid fa-magnifying-glass" />
+          <i class="fa-solid fa-user" />
+          <i class="fa-solid fa-basket-shopping" />
+        </div>
+      </div>
     </nav>
   );
 }
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import './Main.scss';
 
 const Main = () => {
-  return <div />;
+  return (
+    <main>
+      <section>
+        <div className="main-01">
+          <div className="main-01-left">
+            <div className="main-left-txt">
+              <p className="txt-top">#스테디셀러 #미드센츄리 #이탈리아</p>
+              <p className="txt-bottom">혁신적이고 자유로운 가구, Pezzani</p>
+            </div>
+          </div>
+
+          <div className="main-01-right">
+            <div className="right-top">
+              <div className="right-top-child" />
+
+              <p className="right-top-top">
+                미니멀 라이프 <br />
+                DOTTUS
+              </p>
+            </div>
+
+            <div className="right-bottom">
+              <div className="right-bottom-child">
+                <p className="right-top-top">
+                  GUBI Obello Lamp <br />
+                  유리와 빛의 만남
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Main;
