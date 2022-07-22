33d12311260a2a4a2ef0db3168c8199eceb92992
[Github <hjlee5711@naver.com>
2022-07-22 19:36:03 +0900

    ADD:회원가입, 로그인 기능 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -13,24 +13,6 @@ function Nav() {
     <nav>
       <div className="inner">
         <div className="logo">
-<<<<<<< HEAD
-          <img
-            src="https://www.casa.co.kr/images/common/logo_new.gif"
-            alt="logo"
-            width="99"
-          />
-        </div>
-        <ul className="menu">
-          <li>FURNITURE</li>
-          <li>HOMEWARE</li>
-          <li>LIGHTING</li>
-          <li>LIFE</li>
-        </ul>
-        <div className="icon-box">
-          <i class="fa-solid fa-magnifying-glass" />
-          <i class="fa-solid fa-user" />
-          <i class="fa-solid fa-basket-shopping" />
-=======
           <img src="/images/main/logo.png" alt="logo" width="99" />
         </div>
         <ul className="menu">
@@ -46,7 +28,6 @@ function Nav() {
           <i className="fa-solid fa-magnifying-glass" />
           <i className="fa-solid fa-user" />
           <i className="fa-solid fa-basket-shopping" onClick={moveSide} />
->>>>>>> b4db441610680b4ea8539a22bf1fe8d1851e8e38
         </div>
       </div>
     </nav>
