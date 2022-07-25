2f7907769e94bce55ddd9f3c63ddfdf5f95ca5fb
정억화 <jeong-eoghwa@jeong-eoghwaui-MacBookPro.local>
2022-07-25 20:05:33 +0900

    test

--- src/components/MyInfo/MyInfo.js
+++ src/components/MyInfo/MyInfo.js
@@ -1,36 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ShoppingBag from '../ShoppingBag/ShoppingBag';
 import './MyInfo.scss';
 
-const MyInfo = () => {
+const MyInfo = ({ sideOn, setSideOn }) => {
+  let [item, setItem] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      'jwt',
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.TbUpMPmn-RdsST-uVWs8gGmIGv9rT0-jycK1rwVYY3s'
+    );
+
+    fetch('http://10.58.0.74:8000/carts/cart', {
+      method: 'GET',
+      headers: { Authorization: localStorage.getItem('jwt') },
+    })
+      .then(response => response.json())
+      .then(data => setItem(data.carts));
+  }, []);
+
   const removeSide = () => {
-    let bg = document.querySelector('.background');
-    let side = document.querySelector('.quick');
-    bg.style.display = 'none';
-    side.style.right = '-472px';
+    setSideOn('');
   };
+
+  const letOrder = () => {
+    fetch(`http://10.58.0.74:8000/orders/order`, {
+      method: 'POST',
+      headers: { Authorization: localStorage.getItem('jwt') },
+    })
+      .then(response => response.json())
+      .then(() => {
+        setItem([]);
+      });
+  };
+
   return (
     <>
-      <div className="background" />
-      <aside className="quick">
+      <div className={`background ${sideOn}`} />
+      <aside className={`quick ${sideOn}`}>
         <div className="my-name">
           <p className="name">
             <img src="/images/MyInfo/profile-user.png" alt="회원정보" />
             환영합니다 <span>홍길동 고객님</span>
           </p>
-          <div class="close" onClick={removeSide}>
+          <div className="close" onClick={removeSide}>
             <img src="/images/MyInfo/close.png" alt="close" />
           </div>
         </div>
         <div className="my-shopping">
           <h3>
-            장바구니<em id="count">3</em>
+            장바구니<em id="count">{item.length}</em>
           </h3>
           <div className="my-shopping-list">
-            <p> 장바구니에 담긴 상품이 없습니다.</p>
+            {item.map(els => {
+              return (
+                <ShoppingBag itemBag={els} key={els.id} setItem={setItem} />
+              );
+            })}
           </div>
           <div className="my-shopping-btn">
             <div className="viewbtn">장바구니보기</div>
-            <div className="buybtn">바로구매</div>
+            <div
+              className="buybtn"
+              onClick={() => {
+                letOrder();
+              }}
+            >
+              바로구매
+            </div>
           </div>
         </div>
 
